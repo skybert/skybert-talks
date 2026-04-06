4bc0d01708479ae680f9e6cba3f59fb34712a42b
Torstein Krause Johansen <torstein@skybert.net>
2026-04-06 13:30:33 +0200

    Indent the details for code points within the passe string.
    Makes output like this when calling:
    	show_utf8_details("👍🏽")
    
    Output:
            👍🏽 is encoded using 8 bytes
            👍🏽 consists of 2 runes (code points)
                    Byte 0 has code point 128077 U+1F44D '👍'
                    Code point 128077 is encoded using 4 bytes
                    Byte 4 has code point 127997 U+1F3FD '🏽'
                    Code point 127997 is encoded using 4 bytes

--- character-encodings/go/main.go
+++ character-encodings/go/main.go
@@ -10,8 +10,8 @@ func show_utf8_details(s string) {
 	fmt.Printf("%v consists of %v runes (code points)\n", s, utf8.RuneCountInString(s))
 
 	for i, r := range s {
-		fmt.Printf("Byte %d has code point %v U+%X %q\n", i, r, r, r)
-		fmt.Printf("Code point %v is encoded using %v bytes\n", r, utf8.RuneLen(r))
+		fmt.Printf("\tByte %d has code point %v U+%X %q\n", i, r, r, r)
+		fmt.Printf("\tCode point %v is encoded using %v bytes\n", r, utf8.RuneLen(r))
 	}
 
 	fmt.Printf("\n")
@@ -19,16 +19,11 @@ func show_utf8_details(s string) {
 }
 
 func main() {
-	s := "👻"
-	fmt.Printf("one char ghost: %v\n", len(s))
-
 	// codePoint, size := utf8.DecodeRuneInString(s)
 	// fmt.Printf("Character %v has code point %v and uses %v bytes\n", s, codePoint, size)
-
 	show_utf8_details("a")
 	show_utf8_details("å")
 	show_utf8_details("👻")
 	show_utf8_details("👍🏽")
 	show_utf8_details("你好嗎")
-
 }
